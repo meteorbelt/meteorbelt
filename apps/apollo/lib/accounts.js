a32094402f36ca09f8f5a0221e021da4eb37d781
Kyle Finley <git@kylefinley.com>
2013-05-15 19:51:38 -0500

    apollo: account fixes

--- apps/apollo/lib/accounts.js
+++ apps/apollo/lib/accounts.js
@@ -1,6 +1,6 @@
 // Accounts
 // --------
 if (typeof Accounts.emailTemplates !== 'undefined') {
-  Accounts.emailTemplates.siteName = Belt.config.site.name;
-  Accounts.emailTemplates.from = Belt.config.site.email;
+  //Accounts.emailTemplates.siteName = Belt.settings.site.name;
+  //Accounts.emailTemplates.from = Belt.settings.site.email;
 }
