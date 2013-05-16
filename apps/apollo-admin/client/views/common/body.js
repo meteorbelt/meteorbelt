196e614774dee56ab20c9f5fcb2b8281367d3e24
Kyle Finley <git@kylefinley.com>
2013-05-15 19:49:59 -0500

    apollo-admin: cleanup
    remove unused views

--- apps/apollo-admin/client/views/common/body.js
+++
@@ -1,9 +0,0 @@
-Template.body.helpers({
-  layoutName: function () {
-    var isAdmin = Meteor.Router.page().indexOf('admin') === 0;
-    if (isAdmin) {
-      return 'adminLayout';
-    }
-    return 'layout';
-  }
-});
