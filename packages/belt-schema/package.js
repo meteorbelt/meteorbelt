79a99fcec5c26dbc8569ab516d2cfa48090aa777
Kyle Finley <git@kylefinley.com>
2013-06-10 12:08:28 -0500

    belt-schema: wip comment out for now

--- packages/belt-schema/package.js
+++ packages/belt-schema/package.js
@@ -1,18 +1,18 @@
-Package.describe({
-  summary: 'Belt Schema. For use with Meteor Belt applications'
-});
-
-Package.on_use(function (api, where) {
-  api.use('belt');
-  api.use('underscore');
-  api.add_files([
-    'schema_common.js'
-    ], ['client', 'server']);
-});
-
-Package.on_test(function (api) {
-  api.use('belt-schema');
-  api.use('tinytest');
-
-  api.add_files('schema_tests.js', 'client');
-});
+// Package.describe({
+//   summary: 'Belt Schema. For use with Meteor Belt applications'
+// });
+//
+// Package.on_use(function (api, where) {
+//   api.use('belt');
+//   api.use('underscore');
+//   api.add_files([
+//     'schema_common.js'
+//     ], ['client', 'server']);
+// });
+//
+// Package.on_test(function (api) {
+//   api.use('belt-schema');
+//   api.use('tinytest');
+//
+//   api.add_files('schema_tests.js', 'client');
+// });
