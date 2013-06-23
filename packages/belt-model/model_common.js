bd60821d253d5d6c311decc5149c61c7cf783750
Kyle Finley <git@kylefinley.com>
2013-06-22 19:25:53 -0500

    model: models are just models now. Collections will be in belt-collection

--- packages/belt-model/model_common.js
+++ packages/belt-model/model_common.js
@@ -1,66 +1,21 @@
-/**
- * This file incorporates code from 
- * https://github.com/matb33/meteor-collection-hooks
- *
- * (c) 2013 Mathieu Bouchard
- */
-
-// Collection
-// ----------
-
-function getUserId() {
-  var currentInvocation;
-
-  try {
-    currentInvocation = Meteor._CurrentInvocation.get();
-  } catch (e) {}
-
-  if (currentInvocation) {
-    return currentInvocation.userId;
-  } else {
-    // this.userId will likely not be defined because no one usually
-    // invokes collection.insert with a `.call(this)`... But if they
-    // do, userId will be available to the end-callback.
-    return this.userId || null;
+var Model = function (doc, schema) {
+  if (schema) {
+    this._schema = schema;
   }
-}
-
-var delegate = function ( /* arguments */ ) {
-  var self = this;
-  var args = Array.prototype.slice.call(arguments);
-  var type = args.shift();
-  var method = args.shift();
-  _.each(self._processors[method][type], function (fn) {
-    // The function call in an if statement to ensure that it is processed synchronously.
-    // XXX we're setting `this` in the fn to self, because we have to set it to
-    // something, but it should not be relayed upon. Maybe it would be better to set
-    // `this` to the doc?
-    if (fn.apply(self, args)) {
-      return false;
-    }
-  });
-};
-
-var BaseModel = function (doc) {
   _.extend(this, doc);
 };
 
-_.extend(BaseModel.prototype, {
+_.extend(Model.prototype, {
 
-  // added by creator
-  /**
-   * a reference to the parent collection
-   * @type {Belt.Model}
-   */
-  _collection: null,
+  _schema: null,
 
   /**
    * strips unnecessary properties and methods
    * @return {Object}
    */
   toObject: function () {
-    // remove some properties
-    var remove = ['_collection'];
+    // remove some internal properties
+    var remove = ['_collection', '_schema'];
     var doc = _.omit(this, remove);
     // remove all functions
     // _.each(doc, function (v, k) {
@@ -69,6 +24,17 @@ _.extend(BaseModel.prototype, {
     //   }
     // });
     return doc;
+  },
+
+  schema: function (obj) {
+    _.extend(this._schema, obj);
+  },
+
+  populate: function (doc) {
+    if (this._schema) {
+      doc = Belt.Schema.populate(this._schema, doc || {});
+    }
+    _.extend(this, doc);
   },
 
   /**
@@ -80,7 +46,7 @@ _.extend(BaseModel.prototype, {
    *             str: true, // should be of type String
    *             num: 42    // no error
    *           }
-   *           error.details:
+   *           error:
    *           {
    *             str: 'should be String'
    *           }
@@ -88,317 +54,11 @@ _.extend(BaseModel.prototype, {
    *         num is omitted because there was no error.
    */
   validate: function () {
-    var err = this._collection.validate(this.toObject());
-    if (err) { return err.details; }
-    return null;
-  },
-
-  populate: function (doc) {
-    doc = Belt.Schema.populate(this._collection._schema, doc || {});
-    _.extend(this, doc);
-  },
-
-  save: function (fn) {
-    // var self = this;
-    // var err = self.validate();
-    // if (err) {
-    //   if (typeof fn === 'function') {
-    //     fn(err, null);
-    //     return null;
-    //   } else {
-    //     throw new Error('Validation Error');
-    //   }
-    // }
-    // var o = self.toObject();
-    return this._collection.save(this.toObject(), fn);
+    return Belt.Schema.validate(this._schema, this.toObject());
   }
-});
-
-_.extend(Meteor.Collection.prototype, {
-
-  BaseModel: BaseModel,
-
-  _methods: {},
-  _schema: {},
-
-  _processors: {
-    find:    { before: [], after: [] },
-    findOne: { before: [], after: [] },
-    insert:  { before: [], after: [] },
-    update:  { before: [], after: [] },
-    save:    { before: [], after: [] },
-    remove:  { before: [], after: [] }
-  },
-
-  // Public
-  // ------
-  validate: function (doc) {
-    var err = Belt.Schema.validate(this._schema, doc);
-    if (err) {
-      return {
-        error: 401,
-        reason: "Validation Error",
-        details: err
-      };
-    }
-    return null;
-  },
-
-  create: function (doc) {
-    var self = this;
-    // Populate the doc. This will fill in missing values with defaults
-    // and convert things to their proper types.
-    doc = Belt.Schema.populate(this._schema, doc || {});
-
-    var m = new BaseModel();
-    m._collection = this;
-    m.populate(doc);
-    _.extend(m, this.BaseModel, this._methods);
-    return m;
-  },
-
-  insert: function () {
-    var self = this;
-    var args = arguments;
-    var doc = args[0];
-    var cb = args[1];
-    // process before functions.
-    if (delegate.call(self, 'before', 'insert', doc, cb) !== false) {
-      // We put this in an if statement to ensure that preprocessors are run synchronously
-      var err = self.validate(doc);
-      if (err) {
-        cb(err, null);
-        return null;
-      }
-      var id = self.insert.apply(self, arguments);
-      // process after functions
-      delegate.call(self, 'after', 'insert', id && self.findOne({_id: id}) || doc, cb);
-    }
-    return id;
-  },
-
-  update: function (selector, modifier, options, callback) {
-    var result, previous, i, len, stopFiltering;
-    var updateArgumentsRaw = Array.prototype.slice.call(arguments).reverse();
-    var updateArguments = [];
-    var userId = getUserId.call(this);
-
-    if (delegate.call(this, "before", "update", userId, selector, modifier, options, callback) !== false) {
-      previous = this._collection.find(selector, {reactive: false}).fetch();
-
-      // Build an array of the parameters in preparation for Function.apply.
-      // We can't use call here because of the way Meteor.Collection.update
-      // resolves if the last parameter is a callback or not. If we use call,
-      // and the caller didn't pass options, callbacks won't work. We need
-      // to trim any undefined arguments off the end of the arguments array
-      // that we pass.
-      stopFiltering = false;
-      for (i = 0, len = updateArgumentsRaw.length; i < len; i++) {
-        // Skip undefined values until we hit a non-undefined value.
-        // Then accept everything.
-        if (stopFiltering || updateArgumentsRaw[i] !== undefined) {
-          updateArguments.push(updateArgumentsRaw[i]);
-          stopFiltering = true;
-        }
-      }
-
-      updateArguments = updateArguments.reverse();
-
-      result = directUpdate.apply(this, updateArguments);
-      delegate.call(this, "after", "update", userId, selector, modifier, options, previous, callback);
-    }
-
-    return result;
-  },
-
-  remove: function (selector, callback) {
-    var result, previous, userId = getUserId.call(this);
-
-    if (delegate.call(this, "before", "remove", userId, selector, callback) !== false) {
-      previous = this._collection.find(selector, {reactive: false}).fetch();
-      result = directRemove.call(this, selector, callback);
-      delegate.call(this, "after", "remove", userId, selector, previous, callback);
-    }
-
-    return result;
-  },
-
-  save: function (doc, cb) {
-    var self = this;
-    var id;
-    // process before functions
-    if (delegate.call(self, 'before', 'save', doc, cb) !== false) {
-      if (! doc._id) {
-        id = this.insert(doc, cb);
-        // process after functions
-      }
-      // Update
-      else {
-        // Mongo complains if the _id is present on set;
-        id = doc._id;
-        delete doc._id;
-        // TODO maybe we can be smarter about the $set. Only setting changed values
-        id = this.update({_id: id}, {$set: doc}, cb);
-      }
-      delegate.call(self, 'after', 'save', id && self.findOne({_id: id}) || doc, cb);
-    }
-    return id;
-  },
-
-  pluggin: function (fn, opts) {
-    return fn(this, opts);
-  },
-
-  // collection methods
-  statics: function (obj) {
-    _.extend(this, obj);
-  },
-
-  schema: function (obj) {
-    _.extend(this._schema, obj);
-    console.log("schema: ", this._schema);
-    console.log("this: ", this);
-  },
-
-  // model methods
-  methods: function (obj) {
-    console.log("methods this: ", this);
-    _.extend(this._baseModel, obj);
-  }
-});
-
-var Model = function (name, attrs) {
-  var self = this;
-
-  // set default values
-  self._mc = null;
-  self._baseModel = BaseModel;
-  self._schema = {};
-  self._methods = {};
-
-  self._processors = {
-    find:    { before: [], after: [] },
-    findOne: { before: [], after: [] },
-    insert:  { before: [], after: [] },
-    update:  { before: [], after: [] },
-    save:    { before: [], after: [] },
-    remove:  { before: [], after: [] }
-  };
-
-  // console.log("schema - constructor: ", self._schema);
-  self._mc = new Meteor.Collection(name, {
-    transform: function (doc) {
-      return self.create(doc);
-    }
-  });
-
-  // add Meteor.Collection methods
-  var sharedMethods = [
-    "find",
-    "findOne",
-    "insert",
-    "update",
-    "remove",
-    "before",
-    "after",
-    "schema",
-    "statics",
-    "methods",
-    "validate",
-    "create",
-    "save",
-    "allow",
-    "deny"
-  ];
-  _.each(sharedMethods, function (method) {
-    self[method] = function (/* arguments */) {
-      return self._mc[method].apply(self, arguments);
-    };
-  });
-
-  // added attributes from passed in object
-  if (attrs instanceof Object) {
-    var safeMethods = ["schema", "methods", "statics", "before", "after"];
-    _.each(_.pick(attrs, safeMethods), function (obj, method) {
-      self[method].call(self, obj);
-    });
-  }
-  // console.log("schema - constructor - buttom: ", self._schema);
-};
-
-
-if (Meteor.isServer) {
-  var _validatedInsert = Meteor.Collection.prototype._validatedInsert;
-  var _validatedUpdate = Meteor.Collection.prototype._validatedUpdate;
-  var _validatedRemove = Meteor.Collection.prototype._validatedRemove;
-
-  // These are triggered on the server, but only when a client initiates
-  // the method call. They act similarly to observes, but simply hi-jack
-  // _validatedInsert. Additionally, they hi-jack the collection
-  // instance's _collection.insert/update/remove temporarily in order to
-  // maintain validator integrity (allow/deny).
-
-  Meteor.Collection.prototype._validatedInsert = function (userId, doc) {
-    var result, id;
-    var self = this;
-
-    var _insert = self._collection.insert;
-    self._collection.insert = function (doc) {
-      // if (delegate.call(self, "before", "insert", userId, doc) !== false) {
-      //   id = _insert.call(this, doc);
-      //   delegate.call(self, "after", "insert", userId, id && this.findOne({_id: id}) || doc);
-      // }
-    };
-    _validatedInsert.call(self, userId, doc);
-    self._collection.insert = _insert;
-  };
-
-  // Meteor.Collection.prototype._validatedUpdate = function (userId, selector, mutator, options) {
-  //   var result, previous;
-  //   var self = this;
-
-  //   var _update = self._collection.update;
-  //   self._collection.update = function (selector, mutator, options) {
-  //     if (delegate.call(self, "before", "update", userId, selector, mutator, options) !== false) {
-  //       previous = this.find(selector).fetch();
-  //       _update.call(this, selector, mutator, options);
-  //       delegate.call(self, "after", "update", userId, selector, mutator, options, previous);
-  //     }
-  //   };
-  //   _validatedUpdate.call(self, userId, selector, mutator, options);
-  //   self._collection.update = _update;
-  // };
-
-  // Meteor.Collection.prototype._validatedRemove = function (userId, selector) {
-  //   var result, previous;
-  //   var self = this;
-
-  //   var _remove = self._collection.remove;
-  //   self._collection.remove = function (selector) {
-  //     if (delegate.call(self, "before", "remove", userId, selector, previous) !== false) {
-  //       previous = this.find(selector).fetch();
-  //       _remove.call(this, selector);
-  //       delegate.call(self, "after", "remove", userId, selector, previous);
-  //     }
-  //   };
-  //   _validatedRemove.call(self, userId, selector);
-  //   self._collection.remove = _remove;
-  // };
-}
-
-_.each(['before', 'after'], function (process) {
-  Meteor.Collection.prototype[process] = function (obj) {
-    var self = this;
-    _.each(obj, function (cb, method) {
-      if (typeof cb !== 'function') {
-        throw new Error('Expected ' + process + ' ' + method + 
-          ' to be of type Function; not of type ' + (typeof cb));
-      }
-      self._processors[method].before.push(cb);
-    });
-  };
 });
 
 // Exports
 // -------
+
 Belt.Model = Model;
--- packages/belt-model/model_tests.js
+++ packages/belt-model/model_tests.js
@@ -1,236 +1,90 @@
 
-var Posts = this.Posts = new Belt.Model("posts", {
-
-  schema: {
-    title:       { type: String, required: true },
-    body:        { type: String, required: true },
-    publishedAt: { type: Date, required: true },
-    isPublished: { type: Boolean, default: false },
-    loudTitle:   String
-  },
-
-  methods: {
-    uppercaseTitle: function () {
-      return this.title.toUpperCase();
-    }
-  },
-
-  statics: {
-    findByTitle: function (title) {
-      return this.find({title: title});
-    }
-  },
-
-  before: {
-    insert: function (doc, user) {
-      // doc.loudTitle = doc.uppercaseTitle();
-      doc.loudTitle = doc.title.toUpperCase();
-      // doc.publishedAt = doc.publishedAt ? new Date(doc.publishedAt) : null;
-    }
-  },
-
-  after: {
-    insert: function () {
-    }
-  }
-});
-
-if (Meteor.isServer) {
-  // Allow all 
-  Posts.allow({
-    insert: function (userId, docs) {
-      return true;
-    },
-    update: function (userId, docs, fields, modifier) {
-      return true;
-    },
-    remove: function (userId, docs) {
-      return true;
-    }
-  });
-}
-
-// Posts.before({
-//   insert: function (doc, user) {
-//     console.log("doc: ", doc);
-//     // doc.loudTitle = doc.uppercaseTitle();
-//     doc.loudTitle = doc.title.toUpperCase();
-//     // doc.publishedAt = doc.publishedAt ? new Date(doc.publishedAt) : null;
-//   }
-// });
-
-var Comments = this.Comments = new Belt.Model("comments");
-
-
-Comments.schema({
-  owner:       { type: String, required: true },
-  body:        { type: String, required: true }
-});
-
-///////////
-// Tests //
-///////////
-
-// Docs
-
-var p1 = {
-  title: "Hello World",
-  body: "Post Body",
-  publishedAt: '1/1/2001'
-};
-
-// incomplete
-var p2 = {
-  title: "Hello World",
-  body: "Post Body",
-};
-
-var c1 = {
-  title: "Original?",
-  body: "Comment Body"
-};
-
 Tinytest.add('belt - model - Belt.Model is Global', function (test) {
   test.isTrue(typeof Belt.Model !== 'undefined');
 });
 
-Tinytest.add('belt - model - model created', function (t) {
+var postSchema = {
+  title:       { type: String, required: true },
+  body:        { type: String, required: true },
+  publishedAt: { type: Date, required: true },
+  isPublished: { type: Boolean, default: false },
+};
 
-  var p = Posts.create(p1);
-  var c = Comments.create(c1);
+Tinytest.add('belt - model - populate', function (t) {
+  var doc = {
+    title: 1, // will be converted to string
+    body: "Post Body",
+    publishedAt: '7/7/7' // will be converted to Date
+  };
 
-  t.equal(p.title, p1.title);
-  t.equal(p.body, p1.body);
-  // check conversion to type
-  t.equal(p.publishedAt, new Date('1/1/2001'));
+  var expect = {
+    title: "1", // will be converted to string
+    body: "Post Body",
+    publishedAt: new Date('7/7/7') // will be converted to Date
+  };
 
-  t.equal(c.title, c1.title);
-  t.equal(c.body, c1.body);
+  // start off with an empy doc
+  var p = new Belt.Model({}, postSchema);
+  p.populate(doc);
+  t.equal(p.title, expect.title);
+  t.equal(p.body, expect.body);
+  t.equal(p.publishedAt, expect.publishedAt);
 
-  // Test defaults
-  t.equal(p.isPublished, false);
+  // reset
+  p = {};
 
+  // populate on construction
+  // start off with an empy doc
+  p = new Belt.Model(doc, postSchema);
+  t.equal(p.title, expect.title);
+  t.equal(p.body, expect.body);
+  t.equal(p.publishedAt, expect.publishedAt);
 });
 
-Tinytest.add('belt - model - BaseModel validate', function (t) {
-  var now = (new Date())
-  var p = Posts.create(p1);
+Tinytest.add('belt - model - validate', function (t) {
   // valid
-  t.equal(p.validate(), null);
+  // -----
+  var doc1 = {
+    title: "Hello World",
+    body: "Post Body",
+    publishedAt: new Date()
+  };
+
+  var p1 = new Belt.Model(doc1, postSchema);
+  t.equal(p1.validate(), null);
+
   // invalid
-  p.title = 42;
-  p.description = now;
-  p.isPublished = 'yes please';
-  p.publishedAt = 'hello';
+  // -------
+  var doc2 = {
+    title: 42,
+    isPublished: 'yes please',
+    publishedAt: 'hello'
+  };
+  var p2 = new Belt.Model(doc2, postSchema);
   var err = {
     title: "must be a String",
-    publishedAt: "must be a Date",
-    isPublished: "must be a Boolean"
+    body: "required",
+    isPublished: "must be a Boolean",
+    publishedAt: "must be a Date"
   };
-  t.equal(p.validate(), err);
+  t.equal(p2.validate(), err);
 });
 
-Tinytest.add('belt - model - model methods', function (t) {
-  var p = Posts.create(p1);
-  var c = Comments.create(c1);
+Tinytest.add('belt - model - schema', function (t) {
+  var doc1 = {
+    title: "Hello World",
+    body: "Post Body",
+    publishedAt: new Date()
+  };
+  var p = new Belt.Model(doc1, postSchema);
+  p.populate({extra: 'new stuff'});
+  // won't show up because the extra property is not present in the schema
+ 
+  t.equal(p.extra, undefined);
 
-  t.equal(p.uppercaseTitle(), p1.title.toUpperCase());
-  t.equal(c.uppercaseTitle(), c1.title.toUpperCase());
+  // add to schema
+  p.schema({extra: String});
+  // it will show up now because it's been added to the schema
+  p.populate({extra: 'new stuff'});
+  t.equal(p.extra, 'new stuff');
 });
-
-Tinytest.add('belt - model - collection methods', function (t) {
-  var collMethods = [
-    "insert",
-    "update",
-    "remove",
-  ];
-  _.each(collMethods, function (val) {
-    t.isTrue(typeof Posts[val] === 'function'); 
-  });
-  if (Meteor.isServer) {
-    t.isTrue(typeof Posts.allow === 'function'); 
-    t.isTrue(typeof Posts.deny === 'function'); 
-  }
-});
-
-Tinytest.addAsync('belt - model - save invalid', function (t, onComplete) {
-  var p = Posts.create(p2);
-  p.save(function (err, id) {
-    var errMsg = {
-      error: 401,
-      reason: 'Validation Error',
-      details: {
-        publishedAt: 'required'
-      }
-    };
-    t.equal(err, errMsg);
-    onComplete();
-  });
-});
-
-Tinytest.addAsync('belt - model - save valid', function (t, onComplete) {
-  var p = Posts.create(p1);
-  p.publishedAt = new Date();
-  p.save(function (err, id) {
-    // TODO: this return value should the same for client and server. File a bug.
-    if (Meteor.isServer) {
-      t.equal(err, null);
-    } else {
-      t.equal(err, undefined);
-    }
-    t.isTrue(id && id.length);
-    onComplete();
-  });
-});
-
-// this is outside the test to avoid "test" is already defined error
-// when the code twice (once on the server once on the client)
-var M = new Belt.Model("test");
-
-Tinytest.addAsync('belt - model - before after', function (t, onComplete) {
-
-  M.schema({
-    initial:      Number,
-    beforeInsert: Number,
-    afterInsert:  Number
-  });
-
-  M.before({
-    insert: function (doc) {
-      doc.beforeInsert = +(new Date());
-    }
-  });
-
-  M.after({
-    insert: function (doc) {
-      doc.afterInsert = +(new Date());
-    }
-  });
-
-  var m = M.create({initial: +(new Date())});
-
-  if (Meteor.isServer) {
-    debugger;
-    M._mc.insert(m, function (err, id) {
-      console.log("err: ", err);
-      var mm = M.find(id).fetch();
-      t.isTrue(mm.initial <= mm.beforeInsert <= mm.afterInsert);
-      onComplete();
-    });
-  } else {
-    onComplete();
-  }
-
-  // m.save(function (err, id) {
-  //   console.log("m: ", m);
-  //   // m = M.find().fetch();
-  //   // console.log("m: ", m);
-  //   t.isTrue(m.initial < m.beforeInsert < m.afterInsert);
-  //   onComplete();
-  // });
-});
-
-Tinytest.add('belt - model - restricted access', function (t) {
-
-});
-
