cbbe1a1831d6bbcdf43ae62d15d14f0ed1f7e455
Kyle Finley <git@kylefinley.com>
2013-06-25 21:18:24 -0500

    fix(font-awesome): file location

--- packages/belt-font-awesome/package.js
+++ packages/belt-font-awesome/package.js
@@ -3,12 +3,12 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.add_files('font-awesome/build/assets/font-awesome/font/fontawesome-webfont.eot', 'client');
-  api.add_files('font-awesome/build/assets/font-awesome/font/fontawesome-webfont.svg', 'client');
-  api.add_files('font-awesome/build/assets/font-awesome/font/fontawesome-webfont.ttf', 'client');
-  api.add_files('font-awesome/build/assets/font-awesome/font/fontawesome-webfont.woff', 'client');
-  api.add_files('font-awesome/build/assets/font-awesome/font/FontAwesome.otf', 'client');
-  api.add_files('font-awesome/build/assets/font-awesome/css/font-awesome.css', 'client');
+  api.add_files('font-awesome/font/fontawesome-webfont.eot', 'client');
+  api.add_files('font-awesome/font/fontawesome-webfont.svg', 'client');
+  api.add_files('font-awesome/font/fontawesome-webfont.ttf', 'client');
+  api.add_files('font-awesome/font/fontawesome-webfont.woff', 'client');
+  api.add_files('font-awesome/font/FontAwesome.otf', 'client');
+  api.add_files('font-awesome/css/font-awesome.css', 'client');
 
   // font path override
   api.add_files('path-override.css', 'client');
