dce13aedb6a3b3776703732b80dfe22bfad32850
Kyle Finley <git@kylefinley.com>
2013-06-27 13:12:46 -0500

    settings: moved data to data property

--- packages/belt-settings/settings_tests.js
+++ packages/belt-settings/settings_tests.js
@@ -41,44 +41,52 @@ var settings = {
 
 Tinytest.add('belt - settings - objectToList', function (test) {
 
-  var r = Belt.Settings._objectToList(settings);
+  var r = Belt.Settings._objectToList(settings.belt);
   var out = [{
-    "public": true,
     "_id": "site",
-    "name": "Apollo Site",
-    "description": "Web simple",
-    "email": "John Doe <john@example.com>"
-  }, {
     "public": true,
-    "_id": "googleAnalytics",
-    "code": "UA-XXXXXXX-X"
-  }, {
-    "public": true,
-    "_id": "contact",
-    "showAddress": true,
-    "address": {
-      "address1": "1600 Pennsylvania Ave NW",
-      "address2": "",
-      "city": "Washington",
-      "state": "DC",
-      "zipcode": "20500"
-    },
-    "phone": "(555) 111-1111",
-    "fax": "(555) 222-2222",
-    "person": {
-      "name": "John Doe",
-      "email": "john@example.com"
+    "data": {
+      "name": "Apollo Site",
+      "description": "Web simple",
+      "email": "John Doe <john@example.com>"
     }
   }, {
-    "public": false,
+    "_id": "googleAnalytics",
+    "public": true,
+    "data": {
+      "code": "UA-XXXXXXX-X"
+    }
+  }, {
+    "_id": "contact",
+    "public": true,
+    "data": {
+      "showAddress": true,
+      "address": {
+        "address1": "1600 Pennsylvania Ave NW",
+        "address2": "",
+        "city": "Washington",
+        "state": "DC",
+        "zipcode": "20500"
+      },
+      "phone": "(555) 111-1111",
+      "fax": "(555) 222-2222",
+      "person": {
+        "name": "John Doe",
+        "email": "john@example.com"
+      }
+    }
+  }, {
     "_id": "privateVar",
-    "first": 1
+    "public": false,
+    "data": {
+      "first": 1
+    }
   }];
   test.equal(r, out);
 });
 
 Tinytest.add('belt - settings - insertMissing', function (test) {
   // TODO better tests
-  var r = Belt.Settings._insertMissing(Belt.Settings._objectToList(settings));
-  var s = Belt.Settings.find().fetch();
+  // var r = Belt.Settings._insertMissing(Belt.Settings._objectToList(settings));
+  // var s = Belt.Settings.find().fetch();
 });
