95f266b7ea6f57bef3d143d07d97c8778385c222
Kyle Finley <git@kylefinley.com>
2013-07-02 10:11:53 -0500

    apollo-contact: wip

--- packages/apollo-contact/contact/contact.js
+++ packages/apollo-contact/contact/contact.js
@@ -1,48 +1,20 @@
-// TODO move this to a package
-// TODO: make reactive
-this.contactGoogleMapCallback = function () {
-  // TODO Geocoding should be cached; possible in the datastore
-  var mapOptions = {
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("contact-map"), mapOptions);
-  var geocoder = new google.maps.Geocoder();
-  var a = Belt.settings.public.contact.address;
-  var addr = a.address1 + ' ' + a.address2 + ' ' + a.city + ', ' + a.state + ' ' + a.zip;
-  geocoder.geocode({ 'address': addr}, function (results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-        map: map,
-        draggable: false,
-        animation: google.maps.Animation.DROP,
-        position: results[0].geometry.location
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-};
-
-var contactLoadMap = function () {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "http://maps.googleapis.com/maps/api/js?sensor=true&callback=contactGoogleMapCallback";
-  document.body.appendChild(script);
-};
 
 Template.contact.created = function () {
-  // TODO: we should only load the map if it hasn't been load.
-  // The problem is that the callback
-  contactLoadMap();
+  var contact = Belt.Settings.get('contact');
+  if (contact && contact.showAddress) {
+    // contactLoadMap();
+    // Belt.Maps.load('')
+    var a = contact.address;
+    var addr = a.address1 + ' ' + a.address2 + ' ' + a.city + ', ' + a.state + ' ' + a.zip;
+    Belt.Maps.addressToMap('contact-map', addr);
+  }
 };
 
 Template.contact.preserve(["#contact-map"]);
 
 Template.contact.helpers({
   contact: function () {
-    return Belt.Settings.findOne('contact');
+    return Belt.Settings.get('contact');
   }
 });
 
@@ -57,7 +29,8 @@ Template.contact.events({
         Belt.Flash.error(err.reason);
         return;
       }
-      Belt.Flash.success('Your message was sent successfully. Thank for your interest. We will be contacting you soon.');
+      Belt.Flash.success('Your message was sent successfully. ' +
+        'Thank for your interest. We will be contacting you soon.');
       document.getElementById('contact-form').reset();
     });
   }
---
+++ packages/belt-google-maps/package.js
@@ -0,0 +1,13 @@
+Package.describe({
+  summary: "Provides Google Maps. For use with Meteor Belt applications"
+});
+
+Package.on_use(function (api, where) {
+  api.use('belt');
+  api.use('belt-settings');
+
+  // XXX there seems to be a bug preventing this file name
+  // investigate later.
+  // api.add_files('google_maps_client.js', 'client');
+  api.add_files('maps_client.js', 'client');
+});
