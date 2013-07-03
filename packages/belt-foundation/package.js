f2a2cc3153fe88fee6ef71f72a8e13de13fc95af
Kyle Finley <git@kylefinley.com>
2013-07-03 08:45:56 -0500

    submodules: added foundation

---
+++ packages/belt-foundation/package.js
@@ -0,0 +1,25 @@
+Package.describe({
+  summary: "Provides Foundation 4.0 complete with SCSS files. For use with Meteor Belt applications"
+});
+
+Package.on_use(function (api, where) {
+  api.use('belt');
+  api.use('deps', 'client');
+  api.use('jquery', 'client');
+
+  api.add_files([
+    'foundation/js/foundation/foundation.alerts.js',
+    'foundation/js/foundation/foundation.clearing.js',
+    'foundation/js/foundation/foundation.dropdown.js',
+    'foundation/js/foundation/foundation.forms.js',
+    'foundation/js/foundation/foundation.joyride.js',
+    'foundation/js/foundation/foundation.js',
+    'foundation/js/foundation/foundation.magellan.js',
+    'foundation/js/foundation/foundation.orbit.js',
+    'foundation/js/foundation/foundation.placeholder.js',
+    'foundation/js/foundation/foundation.reveal.js',
+    'foundation/js/foundation/foundation.section.js',
+    'foundation/js/foundation/foundation.tooltips.js',
+    'foundation/js/foundation/foundation.topbar.js'
+  ], 'client');
+});
