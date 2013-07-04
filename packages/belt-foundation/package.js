069d4a29945a9632bed14e3ebc28f2e7d0e8520a
Kyle Finley <git@kylefinley.com>
2013-07-04 12:56:27 -0500

    apollo: switched from bootstrap to foundation -- wip.

--- packages/belt-foundation/package.js
+++ packages/belt-foundation/package.js
@@ -8,18 +8,21 @@ Package.on_use(function (api, where) {
   api.use('jquery', 'client');
 
   api.add_files([
+    'foundation/js/vendor/custom.modernizr.js',
+    'foundation/js/foundation/foundation.js',
     'foundation/js/foundation/foundation.alerts.js',
     'foundation/js/foundation/foundation.clearing.js',
+    'foundation/js/foundation/foundation.cookie.js',
     'foundation/js/foundation/foundation.dropdown.js',
     'foundation/js/foundation/foundation.forms.js',
     'foundation/js/foundation/foundation.joyride.js',
-    'foundation/js/foundation/foundation.js',
     'foundation/js/foundation/foundation.magellan.js',
     'foundation/js/foundation/foundation.orbit.js',
-    'foundation/js/foundation/foundation.placeholder.js',
     'foundation/js/foundation/foundation.reveal.js',
     'foundation/js/foundation/foundation.section.js',
     'foundation/js/foundation/foundation.tooltips.js',
-    'foundation/js/foundation/foundation.topbar.js'
+    'foundation/js/foundation/foundation.topbar.js',
+    'foundation/js/foundation/foundation.interchange.js',
+    'foundation/js/foundation/foundation.placeholder.js'
   ], 'client');
 });
