bbc42f36b8ed8bf88b5bc6142649728ecacfe5db
Kyle Finley <git@kylefinley.com>
2013-07-08 08:53:57 -0500

    apollo: limited calls to init foundation.

--- apps/apollo/client/views/common/nav.js
+++ apps/apollo/client/views/common/nav.js
@@ -1,3 +1,8 @@
 Template.nav.rendered = function () {
-  $(document).foundation();
+  // We want to load on the render function because we need the contents of
+  // the dom, but we only want it to load once.
+  if (!this.once) {
+    $(document).foundation('topbar');
+    this.once = true;
+  }
 };
---
+++ apps/apollo/client/views/pages/slideshow.js
@@ -0,0 +1,4 @@
+
+Template.slideshow.rendered = function () {
+  $(document).foundation('orbit');
+};
