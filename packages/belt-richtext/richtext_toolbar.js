171b72c4d0a6bf3412987a52aa4c7612b4a1b9eb
Kyle Finley <git@kylefinley.com>
2013-07-12 12:44:00 -0500

    richtext: switch to foundation

--- packages/belt-richtext/richtext_toolbar.js
+++ packages/belt-richtext/richtext_toolbar.js
@@ -1,14 +1,15 @@
 // XXX we shouldn't be using a global here. Find a better way.
 var global = this;
 
+
 Template.beltRichtextToolbar.events({
-  'click #rt-image-btn': function (e) {
+  'click #rt-image-button': function (e) {
     e.preventDefault();
     Belt.File.pick({
       mimetypes: ['image/*'],
       services: ['COMPUTER']
     }, function (file) {
-      $('#belt-richtext-image-dialog').modal();
+      $('#belt-richtext-image-dialog').foundation('reveal', 'open');
       $('#belt-richtext-image-dialog input[name="image-url"]').val(file.url);
       $('#belt-richtext-image-dialog input[name="name"]').val(file.filename);
     }, function (err) {
@@ -16,7 +17,8 @@ Template.beltRichtextToolbar.events({
     });
   },
 
-  'click #rt-link-btn': function (e) {
+  'click #rt-link-button': function (e) {
+    debugger;
     e.preventDefault();
     $('#belt-richtext-link-dialog').modal();
     $('#belt-richtext-link-dialog input[name="link-url"]').focus();
