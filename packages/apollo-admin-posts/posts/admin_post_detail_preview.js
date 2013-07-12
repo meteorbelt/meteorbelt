10387b816571216cda7146eb83937ad6e525ba46
Kyle Finley <git@kylefinley.com>
2013-07-12 12:44:36 -0500

    apollo admin posts: wip

--- packages/apollo-admin-posts/posts/admin_post_detail_preview.js
+++ packages/apollo-admin-posts/posts/admin_post_detail_preview.js
@@ -4,3 +4,20 @@ Template.adminPostDetailPreview.helpers({
     return Posts.findOne( Session.get('postId'));
   }
 });
+
+Template.adminPostDetailPreview.events({
+  'click .delete': function (e, tmpl) {
+    e.preventDefault();
+    var remove = window.confirm("Are you sure you want to remove this post?");
+    if (remove === true) {
+      var p = Posts.findOne(Session.get('postId'));
+      Posts.remove(p._id, function (err) {
+        if (err) {
+          return Belt.Flash.error(err.reason);
+        }
+        // if no error...
+        Meteor._debug('post deleted successfully');
+      });
+    }
+  }
+});
