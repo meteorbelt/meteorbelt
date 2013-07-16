86945cf17df57b7e460806023ab0ee2eafab0005
Kyle Finley <git@kylefinley.com>
2013-07-15 20:15:03 -0500

    Belt.Error: deleted

--- packages/belt-error/error_tests.js
+++
@@ -1,8 +0,0 @@
-Tinytest.add('belt - error - BeltError', function (test) {
-  test.isTrue(typeof Belt.Error !== 'undefined');
-
-  var e = new Belt.Error('TestError');
-  test.equal(e.name, 'BeltError');
-  test.equal(e.message, 'TestError');
-  test.equal(e.toString(), 'BeltError: TestError');
-});
