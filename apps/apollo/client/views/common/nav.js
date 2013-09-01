1cd839e77b802bc3509e464c187df846e1d08e45
Kyle Finley <git@kylefinley.com>
2013-09-01 09:25:04 -0500

    meteor router -> iron router step1

--- apps/apollo/client/views/common/nav.js
+++
@@ -1,8 +0,0 @@
-Template.nav.rendered = function () {
-  // We want to load on the render function because we need the contents of
-  // the dom, but we only want it to load once.
-  if (!this.once) {
-    $(document).foundation('topbar');
-    this.once = true;
-  }
-};
